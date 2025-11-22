522041d77682cfc7233000cb1fd338e43b252686
juuso-finne <juuso.f@gmail.com>
2025-11-22 21:17:55 +0200

    Established navController

--- app/src/main/java/com/example/tammer_manager/MainActivity.kt
+++ app/src/main/java/com/example/tammer_manager/MainActivity.kt
@@ -4,16 +4,14 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.NavHost
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.navigation.compose.NavHost
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.tammer_manager.ui.screens.AppFrame
+import com.example.tammer_manager.ui.screens.Home
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,10 +26,14 @@ class MainActivity : ComponentActivity() {
 fun App( context: Context) {
     val navController = rememberNavController()
 
-    NavHost(
-        navController,
-        startDestination = ""
-    ) {
-
+    AppFrame() {
+        innerPadding ->
+        NavHost(
+            navController = navController,
+            modifier = Modifier.padding(innerPadding),
+            startDestination = "home"
+        ) {
+            composable ("home") { Home(navController)}
+        }
     }
 }
