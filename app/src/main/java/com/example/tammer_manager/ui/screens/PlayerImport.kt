84fcd1858286954faf9fe46671124adc7666bf59
juuso-finne <juuso.f@gmail.com>
2025-11-25 00:20:14 +0200

    Minor edits to 'import players' screen

--- app/src/main/java/com/example/tammer_manager/ui/screens/PlayerImport.kt
+++ app/src/main/java/com/example/tammer_manager/ui/screens/PlayerImport.kt
@@ -1,26 +1,39 @@
 package com.example.tammer_manager.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
 fun PlayerImport(navController: NavHostController, modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = "Import from:",
+            fontSize = 32.sp
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         Button(
             onClick = {}
         ) {
-            Text("Import from Excel")
+            Text(".xlsx file")
         }
     }
 }
