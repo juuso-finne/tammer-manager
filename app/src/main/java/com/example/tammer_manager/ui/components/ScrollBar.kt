e9dfcec08b3ad17c67b6d557adf00e99db265a3d
juuso-finne <juuso.f@gmail.com>
2025-12-03 22:37:58 +0200

    Finished imported players UI. Added search

---
+++ app/src/main/java/com/example/tammer_manager/ui/components/ScrollBar.kt
@@ -0,0 +1,2 @@
+package com.example.tammer_manager.ui.components
+
--- app/src/main/java/com/example/tammer_manager/ui/screens/EnterPlayers.kt
+++ app/src/main/java/com/example/tammer_manager/ui/screens/EnterPlayers.kt
@@ -1,26 +1,29 @@
 package com.example.tammer_manager.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.tammer_manager.data.interfaces.Player
@@ -33,15 +36,45 @@ fun EnterPlayers(
     vmPlayerPool: PlayerPoolViewModel,
     modifier: Modifier = Modifier
 ) {
-    Column(){
-        Column(modifier = Modifier.weight(0.5f)){
+    val (searchTerm, setSearchTerm) = remember{ mutableStateOf("") }
+    Column(
+        modifier = Modifier.padding(horizontal = 5.dp)
+    ){
+        Column(
+            modifier = Modifier.weight(1f),
+        ){
+            Text(
+                text = "Imported players",
+                style = Typography.headlineSmall,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(bottom = 5.dp)
+                    .fillMaxWidth()
+            )
             PlayerPoolHeader()
             HorizontalDivider()
-            PlayerPool(vmPlayerPool.playerPool.collectAsState().value)
+
+            PlayerPool(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                players = vmPlayerPool.playerPool.collectAsState().value.filter(){ p ->
+                    Regex(
+                        pattern = searchTerm,
+                        option= RegexOption.IGNORE_CASE
+                    ).containsMatchIn(p.fullName)
+                }
+            )
+            SearchBar(
+                searchTerm = searchTerm,
+                setSearchTerm = setSearchTerm
+            )
         }
 
-        Column(modifier = Modifier.weight(0.5f)){
+        HorizontalDivider(thickness = 2.dp)
 
+
+        Column(modifier = Modifier.weight(1f)){
         }
     }
 
@@ -52,10 +85,12 @@ fun PlayerPool(
     players: List<ImportedPlayer>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn() {
-        items(players.size) { i ->
-            PlayerPoolItem(players[i])
-            HorizontalDivider()
+    Box(modifier) {
+        LazyColumn() {
+            items(players.size) { i ->
+                PlayerPoolItem(players[i])
+                HorizontalDivider()
+            }
         }
     }
 }
@@ -91,7 +126,7 @@ fun PlayerPoolItem(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            IconButton(onClick = {}, modifier = Modifier.padding(0.5.dp)) {
+            IconButton(onClick = {}) {
                 Icon(
                     imageVector = Icons.Default.Add,
                     tint = Color.Blue,
@@ -100,6 +135,21 @@ fun PlayerPoolItem(
             }
         }
     }
+}
+
+@Composable
+fun SearchBar(
+    searchTerm: String,
+    setSearchTerm: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        modifier = Modifier.padding(vertical = 5.dp),
+        value = searchTerm,
+        placeholder = {Text("Enter name")},
+        onValueChange = { setSearchTerm(it) },
+        label = {Text("Search player")}
+    )
 }
 
 @Composable
