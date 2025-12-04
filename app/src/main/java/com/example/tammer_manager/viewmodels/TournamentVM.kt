9bd663945b1fa1c9bfd8f2406b01449feccdda41
juuso-finne <juuso.f@gmail.com>
2025-12-04 03:25:03 +0200

    Established Tournament Viewmodel and impemented methods for adding and removing players

---
+++ app/src/main/java/com/example/tammer_manager/viewmodels/TournamentVM.kt
@@ -0,0 +1,44 @@
+package com.example.tammer_manager.viewmodels
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.tammer_manager.data.player_import.ImportedPlayer
+import com.example.tammer_manager.data.tournament_admin.RegisteredPlayer
+import com.example.tammer_manager.data.tournament_admin.Tournament
+import kotlinx.coroutines.flow.StateFlow
+
+class TournamentViewModel(
+    private val savedStateHandle: SavedStateHandle
+): ViewModel(){
+    val activeTournament: StateFlow<Tournament?> = savedStateHandle.getStateFlow(
+        key = "tournament",
+        initialValue =  null
+    )
+    val registeredPlayers:StateFlow<List<RegisteredPlayer>> = savedStateHandle.getStateFlow(
+        key ="registeredPlayers",
+        initialValue = listOf()
+    )
+
+    fun initateTournament(name: String, maxRounds: Int){
+        savedStateHandle["tournament"] = Tournament(name, maxRounds)
+    }
+
+    fun addPlayer(player: ImportedPlayer){
+        val newList = registeredPlayers.value.toMutableList()
+        newList.add(RegisteredPlayer(fullName = player.fullName, rating = player.rating))
+        savedStateHandle["registeredPlayers"] = newList.toList()
+    }
+
+    fun removePlayer(index: Int){
+        val newList = registeredPlayers.value.toMutableList()
+        val roundsCompleted = activeTournament.value?.roundsCompleted ?: 0
+        if(roundsCompleted > 0){
+            savedStateHandle["registeredPlayers"] = newList.mapIndexed { i, p ->
+                if(i == index) p.copy(isActive = false) else p
+            }
+        } else{
+        newList.removeAt(index)
+        }
+        savedStateHandle["registeredPlayers"] = newList.toList()
+    }
+}
