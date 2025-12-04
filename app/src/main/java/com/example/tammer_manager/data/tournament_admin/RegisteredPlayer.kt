94be43bac10ada2fdc1163ac58f1d19c197cf3f8
juuso-finne <juuso.f@gmail.com>
2025-12-04 02:01:41 +0200

    Serialized RegisteredPlayer class

--- app/src/main/java/com/example/tammer_manager/data/tournament_admin/RegisteredPlayer.kt
+++ app/src/main/java/com/example/tammer_manager/data/tournament_admin/RegisteredPlayer.kt
@@ -1,6 +1,10 @@
 package com.example.tammer_manager.data.tournament_admin
 
-data class RegisteredPlayer (val fullName: String, val rating: Int){
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class RegisteredPlayer (val fullName: String, val rating: Int): Parcelable{
     var score = 0f
     var tpn: Int? = null
     var isActive: Boolean = true
