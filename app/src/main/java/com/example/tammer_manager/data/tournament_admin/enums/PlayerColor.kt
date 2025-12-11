4f2985beeb4adf49881fb0676a7974957987d073
juuso-finne <juuso.f@gmail.com>
2025-12-11 11:00:09 +0200

    PlayerColor: Implemented abstract method reverse

--- app/src/main/java/com/example/tammer_manager/data/tournament_admin/enums/PlayerColor.kt
+++ app/src/main/java/com/example/tammer_manager/data/tournament_admin/enums/PlayerColor.kt
@@ -3,6 +3,17 @@ package com.example.tammer_manager.data.tournament_admin.enums
 import androidx.compose.ui.graphics.Color
 
 enum class PlayerColor (val colorValue: Color) {
-    WHITE (colorValue = Color.White),
-    BLACK (colorValue = Color.Black)
+    WHITE (colorValue = Color.White) {
+        override fun reverse(): PlayerColor {
+            return PlayerColor.BLACK
+        }
+    },
+    BLACK (colorValue = Color.Black) {
+        override fun reverse(): PlayerColor {
+            return PlayerColor.WHITE
+        }
+    }
+    ;
+
+    abstract fun reverse(): PlayerColor
 }
