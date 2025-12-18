d33199c4da42de04e9542b56f7fc2b6758cad2d1
juuso-finne <juuso.f@gmail.com>
2025-12-19 00:25:57 +0200

    Cleaned up PassesAbsoluteCriteriaTest using test utilities

--- app/src/test/java/com/example/tammer_manager/data/tournament_admin/pairing/PassesAbsoluteCriteriaTest.kt
+++ app/src/test/java/com/example/tammer_manager/data/tournament_admin/pairing/PassesAbsoluteCriteriaTest.kt
@@ -5,6 +5,8 @@ import com.example.tammer_manager.data.tournament_admin.classes.MatchHistoryItem
 import com.example.tammer_manager.data.tournament_admin.classes.RegisteredPlayer
 import com.example.tammer_manager.data.tournament_admin.enums.ColorPreferenceStrength
 import com.example.tammer_manager.data.tournament_admin.enums.PlayerColor
+import com.example.tammer_manager.utils.generatePlayers
+import com.example.tammer_manager.utils.simulateMatch
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
@@ -12,31 +14,8 @@ class PassesAbsoluteCriteriaTest {
 
     @Test
     fun `Two participants shall not play against each other more than once`() {
-        val playerA = RegisteredPlayer(
-            fullName = "X",
-            rating = 1000,
-            id = 1,
-            tpn = 1,
-            matchHistory = listOf(MatchHistoryItem(
-                opponentId = 2,
-                round = 1,
-                result = 0f,
-                color = PlayerColor.WHITE
-            ))
-        )
-
-        val playerB = RegisteredPlayer(
-            fullName = "Y",
-            rating = 1000,
-            id = 2,
-            tpn = 2,
-            matchHistory = listOf(MatchHistoryItem(
-                opponentId = 1,
-                round = 1,
-                result = 0f,
-                color = PlayerColor.WHITE
-            ))
-        )
+        val (playerA, playerB) = generatePlayers(2)
+        simulateMatch(playerA, playerB, 0f, 1f, 1)
 
         assertThat(passesAbsoluteCriteria(
             candidate =  listOf(Pair(playerA, playerB)),
@@ -47,49 +26,11 @@ class PassesAbsoluteCriteriaTest {
 
     @Test
     fun `Non-topscorers with the same absolute colour preference shall not meet`(){
-        val playerA = RegisteredPlayer(
-            fullName = "X",
-            rating = 1000,
-            id = 1,
-            tpn = 1,
-            matchHistory = listOf(
-                MatchHistoryItem(
-                    opponentId = 3,
-                    round = 1,
-                    result = 0f,
-                    color = PlayerColor.WHITE
-                ),
-                MatchHistoryItem(
-                    opponentId = 4,
-                    round = 2,
-                    result = .5f,
-                    color = PlayerColor.WHITE
-                ),
-            ),
-            score = .5f
-        )
-
-        val playerB = RegisteredPlayer(
-            fullName = "Y",
-            rating = 1000,
-            id = 2,
-            tpn = 2,
-            matchHistory = listOf(
-                MatchHistoryItem(
-                    opponentId = 3,
-                    round = 1,
-                    result = 0f,
-                    color = PlayerColor.WHITE
-                ),
-                MatchHistoryItem(
-                    opponentId = 4,
-                    round = 2,
-                    result = 0f,
-                    color = PlayerColor.WHITE
-                ),
-            ),
-            score = 2f
-        )
+        val (playerA, playerB, playerC) = generatePlayers(3)
+        simulateMatch(playerA, playerC, 0.5f, 1f, 1)
+        simulateMatch(playerA, playerC, 0.5f, 1f, 2)
+        simulateMatch(playerB, playerC, 0.5f, 1f, 1)
+        simulateMatch(playerB, playerC, 0.5f, 1f, 2)
 
         val colorPreferences = mapOf<Int, ColorPreference>(
             Pair(playerA.id, playerA.getColorPreference()),
@@ -106,49 +47,11 @@ class PassesAbsoluteCriteriaTest {
 
     @Test
     fun `Topscorers with the same absolute colour preference can play`(){
-        val playerA = RegisteredPlayer(
-            fullName = "X",
-            rating = 1000,
-            id = 1,
-            tpn = 1,
-            matchHistory = listOf(
-                MatchHistoryItem(
-                    opponentId = 3,
-                    round = 1,
-                    result = 1f,
-                    color = PlayerColor.WHITE
-                ),
-                MatchHistoryItem(
-                    opponentId = 4,
-                    round = 2,
-                    result = 1f,
-                    color = PlayerColor.WHITE
-                ),
-            ),
-            score = 2f
-        )
-
-        val playerB = RegisteredPlayer(
-            fullName = "Y",
-            rating = 1000,
-            id = 2,
-            tpn = 2,
-            matchHistory = listOf(
-                MatchHistoryItem(
-                    opponentId = 3,
-                    round = 1,
-                    result = 1f,
-                    color = PlayerColor.WHITE
-                ),
-                MatchHistoryItem(
-                    opponentId = 4,
-                    round = 2,
-                    result = 1f,
-                    color = PlayerColor.WHITE
-                ),
-            ),
-            score = 2f
-        )
+        val (playerA, playerB, playerC) = generatePlayers(3)
+        simulateMatch(playerA, playerC, 1f, 0f, 1)
+        simulateMatch(playerA, playerC, 1f, 0f, 2)
+        simulateMatch(playerB, playerC, 1f, 0f, 1)
+        simulateMatch(playerB, playerC, 1f, 0f, 2)
 
         val colorPreferences = mapOf<Int, ColorPreference>(
             Pair(playerA.id, playerA.getColorPreference()),
@@ -156,6 +59,14 @@ class PassesAbsoluteCriteriaTest {
         )
 
         assertThat(playerA.getColorPreference()).isEqualTo(
+            ColorPreference(
+                colorBalance = 2,
+                preferredColor = PlayerColor.BLACK,
+                strength = ColorPreferenceStrength.ABSOLUTE
+            )
+        )
+
+        assertThat(playerB.getColorPreference()).isEqualTo(
             ColorPreference(
                 colorBalance = 2,
                 preferredColor = PlayerColor.BLACK,
@@ -173,31 +84,9 @@ class PassesAbsoluteCriteriaTest {
 
     @Test
     fun `Players who have not played before, and don't have absolute color preference, should be accepted`(){
-        val playerA = RegisteredPlayer(
-            fullName = "X",
-            rating = 1000,
-            id = 1,
-            tpn = 1,
-            matchHistory = listOf(MatchHistoryItem(
-                opponentId = 7,
-                round = 1,
-                result = 0f,
-                color = PlayerColor.WHITE
-            ))
-        )
-
-        val playerB = RegisteredPlayer(
-            fullName = "Y",
-            rating = 1000,
-            id = 2,
-            tpn = 2,
-            matchHistory = listOf(MatchHistoryItem(
-                opponentId = 5,
-                round = 1,
-                result = 0f,
-                color = PlayerColor.WHITE
-            ))
-        )
+        val (playerA, playerB, playerC) = generatePlayers(3)
+        simulateMatch(playerA, playerC, 1f, 0f, 1)
+        simulateMatch(playerB, playerC, 1f, 0f, 1)
 
         assertThat(passesAbsoluteCriteria(
             candidate =  listOf(Pair(playerA, playerB)),
