3b990e3001777efae7fe84c3e66163ea6bcca74c
juuso-finne <juuso.f@gmail.com>
2025-12-18 22:46:23 +0200

    SimulateMatch: Made black player nullable

--- app/src/test/java/com/example/tammer_manager/utils/SimulateMatch.kt
+++ app/src/test/java/com/example/tammer_manager/utils/SimulateMatch.kt
@@ -4,20 +4,20 @@ import com.example.tammer_manager.data.tournament_admin.classes.MatchHistoryItem
 import com.example.tammer_manager.data.tournament_admin.classes.RegisteredPlayer
 import com.example.tammer_manager.data.tournament_admin.enums.PlayerColor
 
-fun simulateMatch(playerWhite: RegisteredPlayer, playerBlack: RegisteredPlayer, scoreWhite: Float, scoreBlack: Float, round: Int){
+fun simulateMatch(playerWhite: RegisteredPlayer, playerBlack: RegisteredPlayer?, scoreWhite: Float, scoreBlack: Float, round: Int){
     playerWhite.matchHistory = playerWhite.matchHistory.plusElement(MatchHistoryItem(
-        opponentId = playerBlack.id,
+        opponentId = playerBlack?.id,
         round = round,
         result = scoreWhite,
         color = PlayerColor.WHITE
     ))
     playerWhite.score += scoreWhite
 
-    playerBlack.matchHistory = playerBlack.matchHistory.plusElement(MatchHistoryItem(
+    playerBlack?.matchHistory = playerBlack.matchHistory.plusElement(MatchHistoryItem(
         opponentId = playerWhite.id,
         round = round,
         result = scoreBlack,
         color = PlayerColor.BLACK
     ))
-    playerBlack.score += scoreBlack
+    playerBlack?.score += scoreBlack
 }
