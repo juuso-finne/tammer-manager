57b6dffe54f07475dbedc01198dcdcae9f8fd3ce
juuso-finne <juuso.f@gmail.com>
2025-12-21 03:38:52 +0200

    Implemented color preference comparison

--- app/src/main/java/com/example/tammer_manager/data/tournament_admin/classes/ColorPreference.kt
+++ app/src/main/java/com/example/tammer_manager/data/tournament_admin/classes/ColorPreference.kt
@@ -2,9 +2,17 @@ package com.example.tammer_manager.data.tournament_admin.classes
 
 import com.example.tammer_manager.data.tournament_admin.enums.ColorPreferenceStrength
 import com.example.tammer_manager.data.tournament_admin.enums.PlayerColor
+import kotlin.math.abs
 
 data class ColorPreference(
     val strength: ColorPreferenceStrength = ColorPreferenceStrength.NONE,
     val colorBalance: Int = 0,
     val preferredColor: PlayerColor? = null
-)
+): Comparable<ColorPreference>{
+    override fun compareTo(other: ColorPreference): Int =
+        compareBy<ColorPreference> (
+            { it.strength.ordinal },
+            { abs(it.colorBalance) }
+            )
+            .compare(this, other)
+}
