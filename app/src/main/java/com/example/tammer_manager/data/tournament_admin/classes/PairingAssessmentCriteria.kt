3d8f73b5f1a554bcae2b91066f5160550ecc98db
juuso-finne <juuso.f@gmail.com>
2025-12-21 03:53:37 +0200

    PairingassessmentCriteria: Added KDoc comments to methods

--- app/src/main/java/com/example/tammer_manager/data/tournament_admin/classes/PairingAssessmentCriteria.kt
+++ app/src/main/java/com/example/tammer_manager/data/tournament_admin/classes/PairingAssessmentCriteria.kt
@@ -2,19 +2,19 @@ package com.example.tammer_manager.data.tournament_admin.classes
 
 data class PairingAssessmentCriteria(
 
-    //Minimise the number of unplayed games of the assignee of the pairing-allocated-bye.
+    /**Minimise the number of unplayed games of the assignee of the pairing-allocated-bye.*/
     var pabAssigneeUnplayedGames:Int = 0,
 
-    //Minimise the number of topscorers or topscorers' opponents who get a colour difference higher than +2 or lower than -2
+    /**Minimise the number of topscorers or topscorers' opponents who get a colour difference higher than +2 or lower than -2*/
     var topScorerOrOpponentColorImbalanceCount: Int = 0,
 
-    //Minimise the number of topscorers or topscorers' opponents who get the same colour three times in a row.
+    /**Minimise the number of topscorers or topscorers' opponents who get the same colour three times in a row.*/
     var topScorersOrOpponentsColorstreakCount: Int = 0,
 
-    //Minimise the number of players who do not get their colour preference.
+    /**Minimise the number of players who do not get their colour preference.*/
     var colorpreferenceConflicts: Int = 0,
 
-    //Minimise the number of players who do not get their strong colour preference.
+    /**Minimise the number of players who do not get their strong colour preference.*/
     var strongColorpreferenceConflicts: Int = 0
 ): Comparable<PairingAssessmentCriteria>{
 
