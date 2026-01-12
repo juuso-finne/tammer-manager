2730fb9d5527ace9a8f692898fe94faa81e3bc88
juuso-finne <juuso.f@gmail.com>
2026-01-12 03:41:50 +0200

    Defined nextPermutation and helper functions

---
+++ app/src/main/java/com/example/tammer_manager/data/combinatorics/Permutations.kt
@@ -0,0 +1,14 @@
+package com.example.tammer_manager.data.combinatorics
+
+fun <T: Comparable<T>> popAndShift(list: MutableList<T>, i: Int):Boolean{
+    TODO("To be implemented")
+}
+
+
+fun <T: Comparable<T>> swapWithFirstBigger(list: MutableList<T>, startIndex: Int): Boolean{
+    TODO("To be implemented")
+}
+
+fun <T: Comparable<T>> nextPermutation (list: MutableList<T>, changedIndices: MutableList<Int>, length: Int = list.size): Boolean {
+    TODO("To be implemented")
+}
