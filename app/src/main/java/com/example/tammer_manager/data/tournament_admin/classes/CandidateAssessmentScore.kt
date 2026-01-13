7d643451dbe4b5575a9c900366f5ffd0f2c3f304
juuso-finne <juuso.f@gmail.com>
2026-01-13 23:10:37 +0200

    Created class CandidateAssessmentScore

---
+++ app/src/main/java/com/example/tammer_manager/data/tournament_admin/classes/CandidateAssessmentScore.kt
@@ -0,0 +1,17 @@
+package com.example.tammer_manager.data.tournament_admin.classes
+
+data class CandidateAssessmentScore(
+    val bestTotal: PairingAssessmentCriteria = PairingAssessmentCriteria(
+        pabAssigneeUnplayedGames = Int.MAX_VALUE,
+        topScorerOrOpponentColorImbalanceCount = Int.MAX_VALUE,
+        topScorersOrOpponentsColorstreakCount = Int.MAX_VALUE,
+        colorpreferenceConflicts = Int.MAX_VALUE,
+        strongColorpreferenceConflicts = Int.MAX_VALUE
+    ),
+
+    val bestCandidate : MutableList<Pair<RegisteredPlayer, RegisteredPlayer>> = mutableListOf(),
+
+    val currentTotal: PairingAssessmentCriteria = PairingAssessmentCriteria(),
+
+    val currentIndividualAssessments: MutableList<PairingAssessmentCriteria> = mutableListOf()
+)
