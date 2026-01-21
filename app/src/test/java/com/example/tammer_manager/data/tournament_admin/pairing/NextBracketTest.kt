2d27399c553f7ba25d45868bea26b5451c1852b4
juuso-finne <juuso.f@gmail.com>
2026-01-21 07:28:03 +0200

    Expanded NextBracketTest

--- app/src/test/java/com/example/tammer_manager/data/tournament_admin/pairing/NextBracketTest.kt
+++ app/src/test/java/com/example/tammer_manager/data/tournament_admin/pairing/NextBracketTest.kt
@@ -7,6 +7,8 @@ import com.example.tammer_manager.utils.simulateMatch
 import com.google.common.truth.Truth.assertThat
 import org.junit.Before
 import org.junit.Test
+import kotlin.math.max
+import kotlin.math.min
 
 class NextBracketTest {
 
@@ -16,6 +18,10 @@ class NextBracketTest {
     lateinit var playerD: RegisteredPlayer
     lateinit var playerE: RegisteredPlayer
     lateinit var playerF: RegisteredPlayer
+    lateinit var playerG: RegisteredPlayer
+    lateinit var playerH: RegisteredPlayer
+    lateinit var playerI: RegisteredPlayer
+    lateinit var playerJ: RegisteredPlayer
 
     lateinit var players: MutableList<RegisteredPlayer>
 
@@ -23,7 +29,10 @@ class NextBracketTest {
 
     @Before
     fun setup(){
-        players = generatePlayers(5).toMutableList()
+        players = generatePlayers(10).toMutableList()
+
+        players.sort()
+        
         val (a,b,c,d,e) = players
 
         playerA = a
@@ -32,16 +41,30 @@ class NextBracketTest {
         playerD = d
         playerE = e
 
-        playerF = playerE.copy(fullName = "Player 6", id = 6, tpn = 6)
-        players.add(playerF)
 
-        simulateMatch(playerA, playerB, 1f, 0f, 1)
-        simulateMatch(playerC, playerD, 1f, 0f, 1)
-        simulateMatch(playerE, playerF, 1f, 0f, 1)
+        val (f,g,h,i,j) = players.subList(5, 10)
 
-        simulateMatch(playerA, playerD, 1f, 0f, 2)
-        simulateMatch(playerB, playerE, 1f, 0f, 2)
-        simulateMatch(playerF, playerC, 1f, 0f, 2)
+        playerF = f
+        playerG = g
+        playerH = h
+        playerI = i
+        playerJ = j
+
+        players.removeAt(4)
+
+        simulateMatch(playerF, playerA, 0f, 1f, 1)
+        simulateMatch(playerG, playerB, 1f, 0f, 1)
+        simulateMatch(playerC, playerH, .5f, .5f, 1)
+        simulateMatch(playerI, playerD, 0f, 1f, 1)
+        simulateMatch(playerJ, null, 1f, 0f, 1)
+        playerJ.receivedPairingBye = true
+
+        simulateMatch(playerA, playerG, 0f, 1f, 2)
+        simulateMatch(playerD, playerJ, 1f, 0f, 2)
+        simulateMatch(playerB, playerC, 1f, 0f, 2)
+        simulateMatch(playerH, playerF, 1f, 0f, 2)
+        simulateMatch(playerI, null, 1f, 1f, 2)
+        playerI.receivedPairingBye = true
 
         colorPreferenceMap = players.associateBy(
             {it.id},
@@ -60,6 +83,12 @@ class NextBracketTest {
             roundsCompleted = 2,
             maxRounds = 4,
             incomingDownfloaters = listOf()
+        )
+
+        pairings.sortWith(
+            compareByDescending<Pair<RegisteredPlayer, RegisteredPlayer?>> { max(it.first.score, (it.second?.score ?: 0f)) }
+                .thenByDescending { it.first.score + (it.second?.score ?: 0f)}
+                .thenBy { min(it.first.tpn, (it.second?.tpn ?: 0)) }
         )
 
         pairings.forEachIndexed { i, it ->
