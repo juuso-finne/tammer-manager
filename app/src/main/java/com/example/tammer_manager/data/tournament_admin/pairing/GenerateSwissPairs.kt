74e228e343eb1bae898ed36eceb987c549fd0b3b
juuso-finne <juuso.f@gmail.com>
2026-01-22 17:49:43 +0200

    Implemented assignColors

--- app/src/main/java/com/example/tammer_manager/data/tournament_admin/pairing/GenerateSwissPairs.kt
+++ app/src/main/java/com/example/tammer_manager/data/tournament_admin/pairing/GenerateSwissPairs.kt
@@ -1,10 +1,13 @@
 package com.example.tammer_manager.data.tournament_admin.pairing
 
+import com.example.tammer_manager.data.tournament_admin.classes.ColorPreference
 import com.example.tammer_manager.data.tournament_admin.classes.HalfPairing
 import com.example.tammer_manager.data.tournament_admin.classes.Pairing
 import com.example.tammer_manager.data.tournament_admin.classes.PairingList
 import com.example.tammer_manager.data.tournament_admin.classes.RegisteredPlayer
+import com.example.tammer_manager.data.tournament_admin.enums.ColorPreferenceStrength
 import com.example.tammer_manager.data.tournament_admin.enums.PlayerColor
+import kotlin.math.abs
 
 fun pairingsPlaceholder(players: List<RegisteredPlayer>):PairingList{
     val output = mutableListOf<Pairing>()
@@ -26,4 +29,48 @@ fun pairingsPlaceholder(players: List<RegisteredPlayer>):PairingList{
 
 fun generateSwissPairs(players: List<RegisteredPlayer>): PairingList{
     return pairingsPlaceholder(players)
+}
+
+fun assignColors(players: Pair<RegisteredPlayer, RegisteredPlayer?>, colorPreferenceMap: Map<Int, ColorPreference>):Pairing{
+    val (playerA, playerB) = players
+    val preferenceA = colorPreferenceMap[playerA.id]
+    val preferenceB = colorPreferenceMap[playerB?.id]
+
+    if(preferenceB == null){
+        return mapOf(
+            Pair(PlayerColor.WHITE, HalfPairing(playerID = playerA.id)),
+                    Pair(PlayerColor.BLACK, HalfPairing())
+        )
+    }
+
+    if(preferenceA?.strength == ColorPreferenceStrength.NONE && preferenceB.strength == ColorPreferenceStrength.NONE){
+        val playerAColor = PlayerColor.entries.random()
+
+        return mapOf(
+            Pair(playerAColor, HalfPairing(playerID = playerA.id)),
+            Pair(playerAColor.reverse(), HalfPairing(playerID = playerB?.id))
+        )
+    }
+
+    if(preferenceA?.preferredColor != preferenceB.preferredColor){
+        return mapOf(
+            Pair(preferenceA!!.preferredColor!!, HalfPairing(playerID = playerA.id)),
+            Pair(preferenceB.preferredColor!!, HalfPairing(playerID = playerB?.id))
+        )
+    }
+
+    val preferenceOrder = players.toList().sortedWith(
+        compareByDescending<RegisteredPlayer?>{ colorPreferenceMap[it?.id]?.strength }
+            .thenByDescending { abs(colorPreferenceMap[it?.id]?.colorBalance ?: 0) }
+            .thenByDescending { it?.score }
+            .thenBy { it?.tpn }
+    )
+
+    val preferredPlayerId = preferenceOrder.first()?.id
+    val preferredPlayerColor = colorPreferenceMap[preferredPlayerId]?.preferredColor
+
+    return mapOf(
+        Pair(preferredPlayerColor!!, HalfPairing(playerID = preferredPlayerId)),
+            Pair(preferredPlayerColor.reverse(), HalfPairing(playerID = preferenceOrder.last()?.id))
+    )
 }
