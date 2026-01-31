ef0de75944f735022261ccc688e3040f3d0348b8
juuso-finne <juuso.f@gmail.com>
2026-02-01 00:26:05 +0200

    Added tournament summary to Home screen

--- app/src/main/java/com/example/tammer_manager/ui/screens/Home.kt
+++ app/src/main/java/com/example/tammer_manager/ui/screens/Home.kt
@@ -2,16 +2,22 @@ package com.example.tammer_manager.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.tammer_manager.data.player_import.ImportedPlayer
 import com.example.tammer_manager.data.tournament_admin.classes.Tournament
+import com.example.tammer_manager.ui.theme.Typography
 import com.example.tammer_manager.viewmodels.TournamentViewModel
 
 @Composable
@@ -24,6 +30,26 @@ fun Home(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        val tournament = vmTournament.activeTournament.collectAsState().value
+
+        Text(
+            text = if(tournament != null) "${tournament.name}:" else "No active tournament",
+            style = Typography.bodyLarge
+        )
+
+        Text(
+            text =
+                if (tournament != null && tournament.roundsCompleted < tournament.maxRounds)
+                    "${tournament.roundsCompleted}/${tournament.maxRounds} rounds played"
+                else if(tournament != null) "Tournament complete"
+                else ""
+            ,
+            style = Typography.bodyLarge
+        )
+
+        Spacer(Modifier.height(20.dp))
+
         Button (
             onClick = { navController.navigate("newTournament") }
         ){
