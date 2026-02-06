f5e456c63f89beee1dba4cd189dd387172340e2b
juuso-finne <juuso.f@gmail.com>
2026-02-06 06:52:05 +0200

    Moved components from EnterResults.kt to Shared.kt

--- app/src/main/java/com/example/tammer_manager/ui/screens/enter_results/EnterResults.kt
+++ app/src/main/java/com/example/tammer_manager/ui/screens/enter_results/EnterResults.kt
@@ -1,27 +1,13 @@
 package com.example.tammer_manager.ui.screens.enter_results
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -29,14 +15,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.tammer_manager.data.tournament_admin.classes.HalfPairing
-import com.example.tammer_manager.data.tournament_admin.classes.Pairing
 import com.example.tammer_manager.data.tournament_admin.enums.PlayerColor
 import com.example.tammer_manager.ui.components.ErrorDialog
 import com.example.tammer_manager.ui.components.NoActiveTournament
@@ -160,187 +140,4 @@ fun EnterResults(
             }
         }
     }?: NoActiveTournament()
-}
-
-@Composable
-fun PairingItem(
-    index: Int,
-    vmTournament: TournamentViewModel,
-    pairing: Pairing,
-    modifier: Modifier = Modifier.Companion,
-    borderThickness: Dp = 1.dp,
-    setScore: (Float, Float) -> Unit
-) {
-    val (isMenuOpen, setIsMenuOpen) = remember { mutableStateOf(false) }
-
-    Row(
-        modifier = Modifier.Companion.height(IntrinsicSize.Max),
-        horizontalArrangement = Arrangement.spacedBy(borderThickness * -1)
-    )
-    {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(borderThickness * -1),
-            modifier = Modifier.Companion
-                .fillMaxHeight()
-                .weight(1f)
-        ) {
-            PlayerColor.entries.sortedBy { it.ordinal }.forEach {
-                PlayerScoreRow(
-                    modifier = Modifier.Companion
-                        .fillMaxHeight()
-                        .weight(1f),
-                    color = it,
-                    pairingData = pairing[it],
-                    vmTournament = vmTournament
-                )
-            }
-        }
-
-        Column() {
-            IconButton(
-                onClick = { setIsMenuOpen(!isMenuOpen) },
-                modifier = Modifier.Companion
-                    .background(Color.Companion.Blue)
-                    .fillMaxHeight()
-                    .border(
-                        width = borderThickness,
-                        color = Color.Companion.Black
-                    )
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Menu,
-                    contentDescription = "Enter result for pair ${index + 1}",
-                    tint = Color.Companion.White
-                )
-            }
-            ScoringMenu(
-                isOpen = isMenuOpen,
-                setIsOpen = setIsMenuOpen,
-                modifier = Modifier.Companion,
-                setScore = setScore
-            )
-        }
-    }
-}
-
-@Composable
-fun PlayerScoreRow(
-    vmTournament: TournamentViewModel,
-    color: PlayerColor,
-    pairingData: HalfPairing?,
-    modifier: Modifier = Modifier.Companion,
-    borderThickness: Dp = 1.dp,
-) {
-
-    val player = vmTournament.findPlayerById(pairingData?.playerID ?: 0)
-
-    val score = player?.score ?: 0f
-    val scoreAsText =
-        if (score % 1.0 == 0.0) "%,.0f".format(score)
-        else "%,.1f".format(score)
-
-    Row(
-        modifier = modifier
-            .border(
-                width = borderThickness,
-                color = Color.Companion.Black
-            )
-            .background(color = Color.Companion.White)
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(borderThickness * -1),
-        verticalAlignment = Alignment.Companion.CenterVertically
-    ) {
-
-        Box(
-            modifier = Modifier.Companion
-                .background(color = color.colorValue)
-                .border(width = borderThickness, color = Color.Companion.Black)
-                .fillMaxHeight()
-                .layout() { measurable, constraints ->
-                    val placeable = measurable.measure(constraints)
-                    val currentHeight = placeable.height
-
-                    layout(currentHeight, currentHeight) {
-                        placeable.placeRelative(0, 0)
-                    }
-                }) {}
-
-        Text(
-            text = "${player?.fullName ?: '-'} ($scoreAsText)",
-            maxLines = 1,
-            overflow = TextOverflow.Companion.Ellipsis,
-            modifier = Modifier.Companion
-                .padding(horizontal = 2.dp)
-                .weight(1f),
-            style = Typography.bodyLarge
-        )
-
-        Text(
-            text = when (pairingData?.points) {
-                null -> "-"
-                0.5f -> "½"
-                else -> "%,.0f".format(locale = null, pairingData.points)
-            },
-            maxLines = 1,
-            modifier = Modifier.Companion.padding(horizontal = 2.dp),
-            style = Typography.headlineSmall
-        )
-    }
-}
-
-@Composable
-fun ScoringMenu(
-    setScore: (Float, Float) -> Unit,
-    isOpen: Boolean,
-    setIsOpen: (Boolean) -> Unit,
-    modifier: Modifier = Modifier.Companion
-) {
-    DropdownMenu(
-        expanded = isOpen,
-        onDismissRequest = { setIsOpen(false) },
-    ) {
-        listOf(
-            Pair(1f, 0f),
-            Pair(.5f, .5f),
-            Pair(0f, 1f),
-        ).forEach {
-            ScoringMenuItem(it.first, it.second, setScore = setScore, setIsOpen = setIsOpen)
-            HorizontalDivider()
-        }
-        Spacer(Modifier.Companion.height(20.dp))
-        HorizontalDivider()
-        listOf(
-            Pair(0f, 0f),
-            Pair(.5f, 0f),
-            Pair(0f, .5f),
-        ).forEach {
-            ScoringMenuItem(it.first, it.second, setScore = setScore, setIsOpen = setIsOpen)
-            HorizontalDivider()
-        }
-    }
-}
-
-@Composable
-fun ScoringMenuItem(
-    pointsWhite: Float,
-    pointsBlack: Float,
-    setScore: (Float, Float) -> Unit,
-    setIsOpen: (Boolean) -> Unit,
-    modifier: Modifier = Modifier.Companion
-) {
-    val whitePointsString = if (pointsWhite == 0.5f) "½" else "%,.0f".format(locale = null, pointsWhite)
-    val blackPointsString = if (pointsBlack == 0.5f) "½" else "%,.0f".format(locale = null, pointsBlack)
-
-    DropdownMenuItem(
-        text = {
-            Text(
-                text = "$whitePointsString - $blackPointsString",
-                style = Typography.bodyLarge
-            )
-        },
-        onClick = {
-            setScore(pointsWhite, pointsBlack)
-            setIsOpen(false)
-        }
-    )
 }
