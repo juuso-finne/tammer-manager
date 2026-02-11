80201f24ac63cca0f0464a00abf4f3970e487be2
juuso-finne <juuso.f@gmail.com>
2026-02-11 06:48:50 +0200

    Added type selector to New Torunament screen

--- app/src/main/java/com/example/tammer_manager/ui/screens/NewTournament.kt
+++ app/src/main/java/com/example/tammer_manager/ui/screens/NewTournament.kt
@@ -2,10 +2,14 @@ package com.example.tammer_manager.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -14,10 +18,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.tammer_manager.data.tournament_admin.enums.TournamentType
+import com.example.tammer_manager.ui.theme.Typography
 import com.example.tammer_manager.viewmodels.TournamentViewModel
 import org.apache.commons.lang3.math.NumberUtils
 
@@ -43,6 +49,12 @@ fun NewTorunament(
             onValueChange = { setTournamentName(it) },
         )
 
+        RadioGroupType(
+            modifier = Modifier.padding(horizontal = 10.dp),
+            selectedType = type,
+            setType = setType
+        )
+
         TextField(
             label = { Text("Rounds") },
             value = if (rounds == 0) "" else rounds.toString(),
@@ -56,7 +68,7 @@ fun NewTorunament(
                 navController.navigate("Home")
             },
             enabled =
-                rounds > 0 &&
+                (rounds > 0 || type == TournamentType.ROUND_ROBIN) &&
                 tournamentName.isNotEmpty()
             ,
             modifier = Modifier.padding(top = 10.dp)
@@ -67,6 +79,40 @@ fun NewTorunament(
             onClick = { navController.navigate("Home") }
         ){
             Text("Cancel")
+        }
+    }
+}
+
+@Composable
+fun RadioGroupType(
+    modifier: Modifier = Modifier,
+    selectedType: TournamentType,
+    setType: (TournamentType) -> Unit
+    ){
+    Row (modifier = modifier.fillMaxWidth().selectableGroup(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        TournamentType.entries.forEach {
+            Row(
+                Modifier
+                    .selectable(
+                        selected = (it == selectedType),
+                        onClick = { setType(it) },
+                        role = Role.RadioButton
+                    )
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                RadioButton(
+                    selected = (it == selectedType),
+                    onClick = null
+                )
+                Text(
+                    text = it.toString(),
+                    style = Typography.bodyLarge,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
         }
     }
 }
