2a680d256d7c4da1c6adcd6bce24a8c54cb45ddd
juuso-finne <juuso.f@gmail.com>
2026-02-17 08:40:32 +0200

    Added class TournamentVMState

---
+++ app/src/main/java/com/example/tammer_manager/viewmodels/TournamentVMState.kt
@@ -0,0 +1,13 @@
+package com.example.tammer_manager.viewmodels
+
+import com.example.tammer_manager.data.tournament_admin.classes.PairingList
+import com.example.tammer_manager.data.tournament_admin.classes.RegisteredPlayer
+import com.example.tammer_manager.data.tournament_admin.classes.Tournament
+
+data class TournamentVMState(
+    val activeTournament: Tournament,
+    val registeredPlayers: List<RegisteredPlayer>,
+    val nextPlayerId: Int,
+    val currentRoundPairings: PairingList,
+    val fileName: String
+)
