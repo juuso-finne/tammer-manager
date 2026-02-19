ed58b32dd34db1c2e24600d601159f3bf38f9f13
juuso-finne <juuso.f@gmail.com>
2026-02-19 17:06:29 +0200

    Added methods for finrstIneligiblePair and lastImperfectPair

--- app/src/main/java/com/example/tammer_manager/data/tournament_admin/pairing/swiss/CandidateAssessment.kt
+++ app/src/main/java/com/example/tammer_manager/data/tournament_admin/pairing/swiss/CandidateAssessment.kt
@@ -128,3 +128,53 @@ fun bestPossibleScore(
         strongColorpreferenceConflicts = strongConflicts
     )
 }
+
+fun firstIneligiblePair(
+    pairs: List<Pair<RegisteredPlayer, RegisteredPlayer>>,
+    colorPreferenceMap: Map<Int, ColorPreference>,
+    roundsCompleted: Int,
+    maxRounds: Int
+):Int?{
+    pairs.forEachIndexed{ index, pair ->
+        if (!passesAbsoluteCriteria(
+                candidate = pair,
+                roundsCompleted = roundsCompleted,
+                colorPreferenceMap = colorPreferenceMap,
+                isFinalRound = maxRounds - roundsCompleted <= 1
+        )){
+            return index
+        }
+    }
+    return null
+}
+
+fun lastImperfectPair(
+    pairs: List<Pair<RegisteredPlayer, RegisteredPlayer>>,
+    bestScore: PairingAssessmentCriteria,
+    baseScore: PairingAssessmentCriteria = PairingAssessmentCriteria(),
+    cumulativeScore: PairingAssessmentCriteria,
+    colorPreferenceMap: Map<Int, ColorPreference>,
+    roundsCompleted: Int,
+    maxRounds: Int
+):Int?{
+    var output: Int? = null
+    for(i in pairs.indices.reversed()){
+        val score  = assessPairing(
+            candidate = pairs[i],
+            roundsCompleted = roundsCompleted,
+            maxRounds = maxRounds,
+            colorPreferenceMap = colorPreferenceMap
+        )
+
+        if (output == null && score > PairingAssessmentCriteria()){
+            output = i
+        }
+
+        cumulativeScore += score
+
+        if(cumulativeScore + baseScore >= bestScore){
+            return output
+        }
+    }
+    return output
+}
