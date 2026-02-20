910a0090d76b65a5ce808054544cf55f74ac6e82
juuso-finne <juuso.f@gmail.com>
2026-02-20 13:04:32 +0200

    Removed function assessCandidate as redundant

--- app/src/main/java/com/example/tammer_manager/data/tournament_admin/pairing/swiss/CandidateAssessment.kt
+++ app/src/main/java/com/example/tammer_manager/data/tournament_admin/pairing/swiss/CandidateAssessment.kt
@@ -1,82 +1,12 @@
 package com.example.tammer_manager.data.tournament_admin.pairing.swiss
 
-import com.example.tammer_manager.data.tournament_admin.classes.CandidateAssessmentScore
 import com.example.tammer_manager.data.tournament_admin.classes.ColorPreference
 import com.example.tammer_manager.data.tournament_admin.classes.PairingAssessmentCriteria
 import com.example.tammer_manager.data.tournament_admin.classes.RegisteredPlayer
 import com.example.tammer_manager.data.tournament_admin.enums.ColorPreferenceStrength
 import com.example.tammer_manager.data.tournament_admin.enums.PlayerColor
 import kotlin.math.abs
-import kotlin.math.min
 import kotlin.math.max
-
-fun assessCandidate(
-    s1: List<RegisteredPlayer>,
-    s2: List<RegisteredPlayer>,
-    changedIndices: MutableList<Int>,
-    score: CandidateAssessmentScore,
-    colorPreferenceMap: Map<Int, ColorPreference>,
-    roundsCompleted: Int,
-    maxRounds: Int
-){
-    if (score.currentIndividualAssessments.isEmpty()){
-        for (i in 0 until min(s1.size, s2.size)){
-            val pair = Pair(s1[i], s2[i])
-            if (!passesAbsoluteCriteria(
-                candidate = pair,
-                roundsCompleted = roundsCompleted,
-                colorPreferenceMap = colorPreferenceMap,
-                isFinalRound = maxRounds - roundsCompleted <= 1
-            )){
-                score.resetCurrentScore()
-                return
-            }
-
-            val assessment = assessPairing(
-                candidate = pair,
-                roundsCompleted = roundsCompleted,
-                maxRounds = maxRounds,
-                colorPreferenceMap = colorPreferenceMap
-            )
-
-            score.currentIndividualAssessments.add(assessment)
-            score.currentCandidate.add(pair)
-            score.currentTotal += assessment
-        }
-        score.isValidCandidate = true
-        return
-    }
-
-    for (i in changedIndices){
-        val pair = Pair(s1[i], s2[i])
-        if (!passesAbsoluteCriteria(
-                candidate = pair,
-                roundsCompleted = roundsCompleted,
-                colorPreferenceMap = colorPreferenceMap,
-                isFinalRound = maxRounds - roundsCompleted <= 1
-            )){
-            score.resetCurrentScore()
-            return
-        }
-
-        val newAssessment = assessPairing(
-            candidate = pair,
-            roundsCompleted = roundsCompleted,
-            maxRounds = maxRounds,
-            colorPreferenceMap = colorPreferenceMap
-        )
-
-        val oldAssessment = score.currentIndividualAssessments[i]
-        score.currentTotal -= oldAssessment
-        score.currentTotal += newAssessment
-
-        score.currentIndividualAssessments[i] = newAssessment
-        score.currentCandidate[i] = pair
-    }
-
-    score.isValidCandidate = true
-    return
-}
 
 fun bestPossibleScore(
     players: List<RegisteredPlayer>,
