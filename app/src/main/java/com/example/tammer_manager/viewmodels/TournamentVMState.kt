ede1a9a56ba136ca3589e64f1d2744ac2585d950
juuso-finne <juuso.f@gmail.com>
2026-03-04 16:47:28 +0200

    Made TournamentVMState Parcelable

--- app/src/main/java/com/example/tammer_manager/viewmodels/TournamentVMState.kt
+++ app/src/main/java/com/example/tammer_manager/viewmodels/TournamentVMState.kt
@@ -1,11 +1,14 @@
 package com.example.tammer_manager.viewmodels
 
+import android.os.Parcelable
 import com.example.tammer_manager.data.tournament_admin.classes.PairingList
 import com.example.tammer_manager.data.tournament_admin.classes.RegisteredPlayer
 import com.example.tammer_manager.data.tournament_admin.classes.Tournament
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Parcelize
 data class TournamentVMState(
     val tournament: Tournament,
     val registeredPlayers: List<RegisteredPlayer>,
@@ -13,4 +16,4 @@ data class TournamentVMState(
     val currentRoundPairings: PairingList,
     val isGrouped: Boolean,
     val currentGroup: String,
-)
+): Parcelable
